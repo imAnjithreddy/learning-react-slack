2b3119d7df047236a51eb6cea60a66f017bf7ccc
Anjith Reddy <imanjithreddy@gmail.com>
2018-09-17 13:13:09 +0000

    added login

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,42 +1,47 @@
 import React from 'react';
-import {extendObservable} from 'mobx';
-import {observer} from 'mobx-react';
+import { extendObservable } from 'mobx';
+import { observer } from 'mobx-react';
 import { Button, Input, Container, Header } from 'semantic-ui-react';
+import {graphql} from 'react-apollo';
+import gql from 'graphql-tag';
 
-export default observer(class Login extends React.Component {
-    constructor(props){
-        super(props);
-        extendObservable(this, {
-            email: "",
-            password:""
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    extendObservable(this, {
+      email: '',
+      password: '',
     });
-        
-    }; 
-    
-    onSubmit = () => {
-        const {email, password} = this;
-        console.log(email);
-        console.log(password);
+  }
+
+  onSubmit = async () => {
+    const { email, password } = this;
+    const response = await this.props.mutate({
+      variables: { email, password },
+    });
+    console.log(response);
+    const { ok, token, refreshToken } = response.data.login;
+    if (ok) {
+      localStorage.setItem('token', token);
+      localStorage.setItem('refreshToken', refreshToken);
     }
-    
-    onChange = e => {
-        const {name, value} = e.target;
-        this[name] = value;
-    }
-    render(){
-        const {email, password} = this;
-        return(
-            <Container text>
+  };
+
+  onChange = e => {
+    const { name, value } = e.target;
+    this[name] = value;
+  };
+
+  render() {
+    const { email, password } = this;
+
+    return (
+      <Container text>
         <Header as="h2">Login</Header>
+        <Input name="email" onChange={this.onChange} value={email} placeholder="Email" fluid />
         <Input
-          name="email"
-          onChange={this.onChange}
-          value={email}
-          placeholder="Email"
-          fluid
-        />
-        <Input
-         name="password"
+          name="password"
           onChange={this.onChange}
           value={password}
           type="password"
@@ -45,7 +50,22 @@ export default observer(class Login extends React.Component {
         />
         <Button onClick={this.onSubmit}>Submit</Button>
       </Container>
-            );
-    };
-    
-});
+    );
+  }
+}
+
+const loginMutation = gql`
+  mutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      ok
+      token
+      refreshToken
+      errors {
+        path
+        message
+      }
+    }
+  }
+`;
+
+export default graphql(loginMutation)(observer(Login));
